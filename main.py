cd7b0a1185bd98b2f27f7b574d01168f9cb6baa8
syed oOsama <Syedosama8887@gmail.com>
2024-02-08 15:29:39 -0800

    first comit

---
+++ main.py
@@ -0,0 +1,11 @@
+from functions import ExchangePriceChecker
+
+if __name__ == "__main__":
+    # Get user input for the exchange
+    user_exchange = input("Enter the exchange you want to use (e.g., 'bybit', 'binance'): ")
+
+    # Create an instance of the ExchangePriceChecker class for the specified exchange
+    price_checker = ExchangePriceChecker(user_exchange)
+
+    # Call the main method to execute the functionality
+    price_checker.ma
